4b233891e164297d1961e9ea9d2e2c1433409356
Jia-Baos <jiabd@seer-group.com>
2025-02-12 10:07:54 +0800

    test hikang camera, passed

--- src/hikang/hikang.cpp
+++ src/hikang/hikang.cpp
@@ -5,11 +5,13 @@
 
 bool PrintDeviceInfo(MV_CC_DEVICE_INFO *pstMVDevInfo)
 {
-    if (pstMVDevInfo == nullptr) {
+    if (pstMVDevInfo == nullptr)
+    {
         printf("The Pointer of pstMVDevInfo is NULL!\n");
         return false;
     }
-    if (pstMVDevInfo->nTLayerType == MV_GIGE_DEVICE) {
+    if (pstMVDevInfo->nTLayerType == MV_GIGE_DEVICE)
+    {
         int nIp1 = ((pstMVDevInfo->SpecialInfo.stGigEInfo.nCurrentIp & 0xff000000) >> 24);
         int nIp2 = ((pstMVDevInfo->SpecialInfo.stGigEInfo.nCurrentIp & 0x00ff0000) >> 16);
         int nIp3 = ((pstMVDevInfo->SpecialInfo.stGigEInfo.nCurrentIp & 0x0000ff00) >> 8);
@@ -19,26 +21,38 @@ bool PrintDeviceInfo(MV_CC_DEVICE_INFO *pstMVDevInfo)
         printf("Device Model Name: %s\n", pstMVDevInfo->SpecialInfo.stGigEInfo.chModelName);
         printf("CurrentIp: %d.%d.%d.%d\n", nIp1, nIp2, nIp3, nIp4);
         printf("UserDefinedName: %s\n\n", pstMVDevInfo->SpecialInfo.stGigEInfo.chUserDefinedName);
-    } else if (pstMVDevInfo->nTLayerType == MV_USB_DEVICE) {
+    }
+    else if (pstMVDevInfo->nTLayerType == MV_USB_DEVICE)
+    {
         printf("Device Model Name: %s\n", pstMVDevInfo->SpecialInfo.stUsb3VInfo.chModelName);
         printf("UserDefinedName: %s\n\n", pstMVDevInfo->SpecialInfo.stUsb3VInfo.chUserDefinedName);
-    } else if (pstMVDevInfo->nTLayerType == MV_GENTL_GIGE_DEVICE) {
+    }
+    else if (pstMVDevInfo->nTLayerType == MV_GENTL_GIGE_DEVICE)
+    {
         printf("UserDefinedName: %s\n", pstMVDevInfo->SpecialInfo.stGigEInfo.chUserDefinedName);
         printf("Serial Number: %s\n", pstMVDevInfo->SpecialInfo.stGigEInfo.chSerialNumber);
         printf("Model Name: %s\n\n", pstMVDevInfo->SpecialInfo.stGigEInfo.chModelName);
-    } else if (pstMVDevInfo->nTLayerType == MV_GENTL_CAMERALINK_DEVICE) {
+    }
+    else if (pstMVDevInfo->nTLayerType == MV_GENTL_CAMERALINK_DEVICE)
+    {
         printf("UserDefinedName: %s\n", pstMVDevInfo->SpecialInfo.stCMLInfo.chUserDefinedName);
         printf("Serial Number: %s\n", pstMVDevInfo->SpecialInfo.stCMLInfo.chSerialNumber);
         printf("Model Name: %s\n\n", pstMVDevInfo->SpecialInfo.stCMLInfo.chModelName);
-    } else if (pstMVDevInfo->nTLayerType == MV_GENTL_CXP_DEVICE) {
+    }
+    else if (pstMVDevInfo->nTLayerType == MV_GENTL_CXP_DEVICE)
+    {
         printf("UserDefinedName: %s\n", pstMVDevInfo->SpecialInfo.stCXPInfo.chUserDefinedName);
         printf("Serial Number: %s\n", pstMVDevInfo->SpecialInfo.stCXPInfo.chSerialNumber);
         printf("Model Name: %s\n\n", pstMVDevInfo->SpecialInfo.stCXPInfo.chModelName);
-    } else if (pstMVDevInfo->nTLayerType == MV_GENTL_XOF_DEVICE) {
+    }
+    else if (pstMVDevInfo->nTLayerType == MV_GENTL_XOF_DEVICE)
+    {
         printf("UserDefinedName: %s\n", pstMVDevInfo->SpecialInfo.stXoFInfo.chUserDefinedName);
         printf("Serial Number: %s\n", pstMVDevInfo->SpecialInfo.stXoFInfo.chSerialNumber);
         printf("Model Name: %s\n\n", pstMVDevInfo->SpecialInfo.stXoFInfo.chModelName);
-    } else {
+    }
+    else
+    {
         printf("Not support.\n");
     }
 
@@ -54,23 +68,27 @@ HiKangCamera::~HiKangCamera()
     int nRet{};
     // 停止取流 | end grab image
     nRet = MV_CC_StopGrabbing(handle_);
-    if (MV_OK != nRet) {
+    if (MV_OK != nRet)
+    {
         printf("MV_CC_StopGrabbing fail! nRet [%x]\n", nRet);
     }
 
     // 销毁句柄 | destroy handle
     nRet = MV_CC_DestroyHandle(handle_);
-    if (MV_OK != nRet) {
+    if (MV_OK != nRet)
+    {
         printf("MV_CC_DestroyHandle fail! nRet [%x]\n", nRet);
     }
     handle_ = nullptr;
 
-    if (handle_ != nullptr) {
+    if (handle_ != nullptr)
+    {
         MV_CC_DestroyHandle(handle_);
         handle_ = nullptr;
     }
 
-    if (data_) {
+    if (data_)
+    {
         free(data_);
         data_ = nullptr;
     }
@@ -94,7 +112,8 @@ bool HiKangCamera::Wait4Device()
     int nRet{};
     // ch:初始化SDK | en:Initialize SDK
     nRet = MV_CC_Initialize();
-    if (MV_OK != nRet) {
+    if (MV_OK != nRet)
+    {
         printf("Initialize SDK fail! nRet [0x%x]\n", nRet);
     }
 
@@ -105,14 +124,18 @@ bool HiKangCamera::Wait4Device()
     nRet = MV_CC_EnumDevices(
         MV_GIGE_DEVICE | MV_USB_DEVICE | MV_GENTL_CAMERALINK_DEVICE | MV_GENTL_CXP_DEVICE | MV_GENTL_XOF_DEVICE,
         &stDeviceList);
-    if (MV_OK != nRet) {
+    if (MV_OK != nRet)
+    {
         printf("MV_CC_EnumDevices fail! nRet [%x]\n", nRet);
     }
-    if (stDeviceList.nDeviceNum > 0) {
-        for (int i = 0; i < stDeviceList.nDeviceNum; i++) {
+    if (stDeviceList.nDeviceNum > 0)
+    {
+        for (int i = 0; i < stDeviceList.nDeviceNum; i++)
+        {
             printf("[device %d]:\n", i);
             MV_CC_DEVICE_INFO *pDeviceInfo = stDeviceList.pDeviceInfo[i];
-            if (pDeviceInfo == nullptr) {
+            if (pDeviceInfo == nullptr)
+            {
                 continue;
             }
 
@@ -123,16 +146,20 @@ bool HiKangCamera::Wait4Device()
             std::string nIp4 = std::to_string(pDeviceInfo->SpecialInfo.stGigEInfo.nCurrentIp & 0x000000ff);
             std::string found_ip = nIp1.append(".").append(nIp2).append(".").append(nIp3).append(".").append(nIp4);
 
-            if (ip_ == found_ip) {
+            if (ip_ == found_ip)
+            {
                 nRet = MV_CC_CreateHandle(&handle_, pDeviceInfo);
-                if (MV_OK != nRet) {
+                if (MV_OK != nRet)
+                {
                     printf("Create Handle Failed!\n");
                     return false;
                 }
                 return true;
             }
         }
-    } else {
+    }
+    else
+    {
         printf("Find No Devices!\n");
     }
 
@@ -145,12 +172,14 @@ bool HiKangCamera::Init()
 
     // 打开设备 | open device
     nRet = MV_CC_OpenDevice(handle_);
-    if (MV_OK != nRet) {
+    if (MV_OK != nRet)
+    {
         printf("MV_CC_OpenDevice fail! nRet [%x]\n", nRet);
 
         // 未成功打开设备则关闭句柄
         nRet = MV_CC_DestroyHandle(handle_);
-        if (MV_OK != nRet) {
+        if (MV_OK != nRet)
+        {
             printf("Destory handle failed\n");
         }
         return false;
@@ -159,24 +188,30 @@ bool HiKangCamera::Init()
     // ch:探测网络最佳包大小(只对GigE相机有效) | en:Detection network optimal package size(It only works for the GigE
     // camera)
     int nPacketSize = MV_CC_GetOptimalPacketSize(handle_);
-    if (nPacketSize > 0) {
+    if (nPacketSize > 0)
+    {
         nRet = MV_CC_SetIntValueEx(handle_, "GevSCPSPacketSize", nPacketSize);
-        if (nRet != MV_OK) {
+        if (nRet != MV_OK)
+        {
             printf("Warning: Set Packet Size fail nRet [0x%x]!\n", nRet);
         }
-    } else {
+    }
+    else
+    {
         printf("Warning: Get Packet Size fail nRet [0x%x]!\n", nPacketSize);
     }
 
     // 设置触发模式为off | set trigger mode as off
     nRet = MV_CC_SetEnumValue(handle_, "TriggerMode", MV_TRIGGER_MODE_OFF);
-    if (MV_OK != nRet) {
+    if (MV_OK != nRet)
+    {
         printf("MV_CC_SetTriggerMode fail! nRet [%x]\n", nRet);
     }
 
     // 设置图像格式
     nRet = MV_CC_SetEnumValue(handle_, "PixelFormat", PixelType_Gvsp_BGR8_Packed);
-    if (MV_OK != nRet) {
+    if (MV_OK != nRet)
+    {
         printf("Set image type fail! nRet [%x]\n", nRet);
     }
 
@@ -184,15 +219,18 @@ bool HiKangCamera::Init()
     MVCC_INTVALUE stParam;
     memset(&stParam, 0, sizeof(MVCC_INTVALUE));
     nRet = MV_CC_GetIntValue(handle_, "PayloadSize", &stParam);
-    if (MV_OK != nRet) {
+    if (MV_OK != nRet)
+    {
         printf("Get PayloadSize fail! nRet [0x%x]\n", nRet);
 
         int nRet = MV_CC_CloseDevice(handle_);
-        if (MV_OK != nRet) {
+        if (MV_OK != nRet)
+        {
             printf("Close device failed\n");
         }
         nRet = MV_CC_DestroyHandle(handle_);
-        if (MV_OK != nRet) {
+        if (MV_OK != nRet)
+        {
             printf("Destory handle failed\n");
         }
         return false;
@@ -201,13 +239,15 @@ bool HiKangCamera::Init()
     // 获得图像所需内存大小并开辟内存
     data_size_ = stParam.nCurValue;
     data_ = (unsigned char *)malloc(sizeof(unsigned char) * stParam.nCurValue);
-    if (data_ == nullptr) {
+    if (data_ == nullptr)
+    {
         printf("pData is null\n");
     }
 
     // 开始取流 | start grab image
     nRet = MV_CC_StartGrabbing(handle_);
-    if (MV_OK != nRet) {
+    if (MV_OK != nRet)
+    {
         printf("Start grabbing failed\n");
         return false;
     }
@@ -217,31 +257,33 @@ bool HiKangCamera::Init()
 
 void HiKangCamera::Run()
 {
-    while (true) {
-        if (handle_ == nullptr) {
+    while (true)
+    {
+        if (handle_ == nullptr)
+        {
             std::stringstream ss;
-            ss << "get frame fail!";
+            ss << "handle_ is nullptr, can't get frame!";
             std::cout << ss.str() << std::endl;
             std::this_thread::sleep_for(std::chrono::milliseconds(1000));
             continue;
-        } else {
+        }
+        else
+        {
             break;
         }
     }
 
-    MV_FRAME_OUT_INFO_EX stImageInfo = { 0 };
+    MV_FRAME_OUT_INFO_EX stImageInfo = {0};
     memset(&stImageInfo, 0, sizeof(MV_FRAME_OUT_INFO_EX));
     auto start = std::chrono::high_resolution_clock::now();
-    while (true) {
+    while (true)
+    {
         int nRet{};
         nRet = MV_CC_GetOneFrameTimeout(handle_, data_, data_size_, &stImageInfo, 100);
-        if (nRet != MV_OK) {
-            std::stringstream ss;
-            ss << "get color frame failed!";
-            std::cout << ss.str() << std::endl;
-            continue;
-        } else if (nRet == MV_OK) {
-            try {
+        if (nRet == MV_OK)
+        {
+            try
+            {
                 printf("Now you GetOneFrame, Width[%d], Height[%d], nFrameNum[%d]\n\n", stImageInfo.nWidth, stImageInfo.nHeight, stImageInfo.nFrameNum);
 
                 {
@@ -260,11 +302,20 @@ void HiKangCamera::Run()
                 ss << "frame freq is " << freq << " ms";
                 std::cout << ss.str() << std::endl;
             }
-            catch (std::exception &e) {
+            catch (std::exception &e)
+            {
                 std::stringstream ss;
                 ss << "handle frame err " << e.what();
                 std::cout << ss.str() << std::endl;
             }
+        }
+
+        else
+        {
+            std::stringstream ss;
+            ss << "get color frame failed!";
+            std::cout << ss.str() << std::endl;
+            continue;
         }
     }
 }
@@ -272,7 +323,8 @@ void HiKangCamera::Run()
 std::optional<cv::Mat> HiKangCamera::GetImg()
 {
     cgu::READ_LOCK(this->img_lock_);
-    if (depth_.empty()) {
+    if (img_.empty())
+    {
         return std::nullopt;
     }
     return img_;
@@ -307,10 +359,13 @@ std::optional<bool> HiKangCamera::SaveImg()
 {
     cv::Mat img{};
     auto res = GetImg();
-    if (res.has_value()) {
+    if (res.has_value())
+    {
         img = res.value();
         std::cout << "save color img" << std::endl;
-    } else {
+    }
+    else
+    {
         std::cout << "save color img failed" << std::endl;
     }
 
--- src/run/camera_manger.cpp
+++ src/run/camera_manger.cpp
@@ -4,32 +4,37 @@
 
 int main()
 {
-    // HiKangCamera cam_hikang;
-    // cam_hikang.Wait4Device();
-    // cam_hikang.Init();
+    HiKangCamera cam_hikang;
+    cam_hikang.SetIP("192.168.192.254");
+    cam_hikang.Wait4Device();
+    cam_hikang.Init();
 
-    // std::thread cam_hikang_thread = std::thread([&cam_hikang]() {
-    //     cam_hikang.Run();
-    // });
-    // cam_hikang_thread.detach();
+    std::thread cam_hikang_thread = std::thread([&cam_hikang]()
+                                                { cam_hikang.Run(); });
+    cam_hikang_thread.detach();
 
-    OrbbecCamera cam_orbbec;
-    cam_orbbec.Wait4Device();
-    cam_orbbec.Init();
-
-    std::thread cam_orbbec_thread = std::thread([&cam_orbbec]() {
-        cam_orbbec.Run();
-    });
-    cam_orbbec_thread.detach();
-
-    while (true) {
+    while (true)
+    {
         std::this_thread::sleep_for(std::chrono::milliseconds(200));
-        // cam_hikang.SaveImg();
-        // cam_hikang.SaveDepth();
-
-        cam_orbbec.SaveImg();
-        cam_orbbec.SaveDepth();
+        cam_hikang.SaveImg();
+        cam_hikang.SaveDepth();
     }
+
+    // OrbbecCamera cam_orbbec;
+    // cam_orbbec.Wait4Device();
+    // cam_orbbec.Init();
+
+    // std::thread cam_orbbec_thread = std::thread([&cam_orbbec]()
+    //                                             { cam_orbbec.Run(); });
+    // cam_orbbec_thread.detach();
+
+    // while (true)
+    // {
+    //     std::this_thread::sleep_for(std::chrono::milliseconds(200));
+
+    //     cam_orbbec.SaveImg();
+    //     cam_orbbec.SaveDepth();
+    // }
 
     return 0;
 }
